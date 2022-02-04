9d7e44b4518eb7bd96e4c7f56263a6cfbb457c33
xis004 <xis004@ucsd.edu>
2022-02-04 15:21:32 -0800

    Test1

--- Test1.java
+++ Test1.java
@@ -1,4 +1,7 @@
-class Test{
+class Test1{
+  public static int add(){
+    return 2;
+  }
   public static void main(String[] args){
     System.out.println("Hello.");
   }
---
+++ TestofTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class TestofTest {
+    @Test
+    public void addition() {
+        assertEquals(2, Test1.add());
+    }
+}
